3613d26ba8ca10bb320ed78621b2bfe8d32b0d2f
brenopapa <bpapa@outlook.com.br>
2024-08-30 18:29:33 -0300

    avoid multithreading locking

--- src/2_receive.py
+++ src/2_receive.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import threading
+from time import sleep
 
 from dotenv import dotenv_values
 from lib.consumer import consumer
@@ -10,17 +11,21 @@ config = dotenv_values(".env")
 
 def main():
 
-    t0 = threading.Thread(target=consumer, args=("customer",))
-    t1 = threading.Thread(target=consumer, args=("product",))
-    t2 = threading.Thread(target=consumer, args=("purchase",))
+    t0 = threading.Thread(target=consumer, args=("customer",), daemon=True)
+    t1 = threading.Thread(target=consumer, args=("product",), daemon=True)
+    t2 = threading.Thread(target=consumer, args=("purchase",), daemon=True)
 
     t0.start()
     t1.start()
     t2.start()
 
-    t0.join()
-    t1.join()
-    t2.join()
+    try:
+        while (t0.is_alive() or t1.is_alive() or t2.is_alive()):
+            sleep(1)
+        print("Thread finished task, exiting")
+    except KeyboardInterrupt:
+        print("[X] Closing main-thread.This will also close the background thread because is set as daemon.")
+    return 0
 
 if __name__ == "__main__":
     try:
